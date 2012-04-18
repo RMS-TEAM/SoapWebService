54b637fc5c17e6af120eb959c8391a8d7b84dac4
James Benitez <michinbenitez@gmail.com>
2012-04-18 09:03:54 -0500

    cambio en nombres

--- ConsumidorRails/app/controllers/user_controller.rb
+++ ConsumidorRails/app/controllers/user_controller.rb
@@ -6,7 +6,7 @@ class UserController < ApplicationController
     @cedula = params[:cedula]
     @pass = params[:password]
     @client = soap_service
-    respuesta = @client.request :web, :ingresar, body: {"arg0"=> @cedula, "arg1" => @pass}
+    respuesta = @client.request :web, :ingresar, :body=> {"arg0"=> @cedula, "arg1" => @pass}
     if respuesta.success?
       r = respuesta.to_array(:ingresar_response).first
       if r
